e038546cec0c2ca65090614048f06813af02409e
Leon Dail <leondailofficial@gmail.com>
2019-08-31 12:40:56 -0700

    wrote bio

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ function addBio(add) {
   if (add) {
     $('main').html(`
         <article class="information">
-            <h3 class="active">bio</h3>
+            <h3 class="active" id="bio">bio</h3>
             <p class="bold">Hi! My name is Leon, and I'm a full-stack web developer.</p>
             <p>I find myself most proud when I am able to solve a challenging problem. I am confident knowing that I can look at a question from all sides and execute the best solution.</p>
-            <p class="special">
-                <h4>skills<h4>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>jQuery</li>
-                    <li>API</li>
+            <p>I have many years experience working with HTML and CSS. In the past, I was the designer and administrator of small, hobby websites, and taught myself how to keep a website running.</p>
+                <ul id="skills">
+                    <li id="skill-html"><img src="html5.png" alt="HTML 5" /></li>
+                    <li id="skill-css"><img src="css.png" alt="CSS 3" /></li>
+                    <li id="skill-javascript"><img src="javascript.png" alt="JavaScript" /></li>
+                    <li id="skill-jQuery"><img src="jquery.png" alt="jQuery" /></li>
+                    <li id="skill-api"><img src="api.png" alt="API" /></li>
                 </ul>
-            </p>
         </article>
         <article class="information">
-            <h3 class="active">projects</h3>
+            <h3 class="active" id="projects">projects</h3>
             <p class="bold">Hi! My name is Leon, and I'm a full-stack web developer.</p>
             <p>Cras vitae augue volutpat risus gravida tincidunt. Suspendisse et tincidunt lorem. Praesent molestie mattis sem, rhoncus fermentum mi placerat ut. Etiam eu sapien lacus. Donec pretium cursus nisi, eu dictum purus efficitur quis. </p>
         </article>`
@@ -41,8 +39,8 @@ function fillNav(add) {
   else {
     $('nav').html(
       `<ul>
-        <li>bio</li>
-        <li>projects</li>
+        <li><a href="#">bio</a></li>
+        <li><a href="#">projects</a></li>
         <li>contact</li>
       </ul>`);
   }
@@ -77,13 +75,26 @@ function ready() {
   console.log($(window).scrollTop());
   $(window).scroll( function() {
     if ($(this).scrollTop() > 0) {
-      showSecondPage();
-      console.log($(this).scrollTop());
+      if ($('body').hasClass('first')) {
+        showSecondPage();
+        console.log($(this).scrollTop());
+      }
     }
     else {
       showFirstPage();
     }
   });
+  $('nav').on('click','a', function(e) {
+    $(this).scrollView();
+  });
 }
 
 $(ready);
+
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+}
