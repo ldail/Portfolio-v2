53ef983d6a2ae17d2bdabe4258c0c1a53a903a4d
Leon Dail <leondailofficial@gmail.com>
2019-09-01 12:17:42 -0700

    compressed images

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function addMain(add) {
             <div class="project-info">
                 <div class="project-info-text">
                     <h4 class="project-name">Your Bookmarks</h4>
-                    <h5 class="project-description">Keep up to tabs with your favorite websites</h5>
+                    <h5 class="project-description">Keep tabs of your favorite websites</h5>
                     <ul class="view-code">
-                        <li class="github-code"><a href="https://github.com/thinkful-ei-gecko/leon-dail-bookmarks-app"><img src="images/github-code.png" alt="View on Github" /></a></li>
-                        <li class="deploy-code"><a href="https://thinkful-ei-gecko.github.io/leon-dail-bookmarks-app/"><img src="images/eye.png" alt="View Deployment" /></a></li>
+                        <li class="github-code"><a href="https://github.com/thinkful-ei-gecko/leon-dail-bookmarks-app"><img src="images/github.png" alt="View on Github" /></a></li>
+                        <li class="deploy-code"><a href="https://thinkful-ei-gecko.github.io/leon-dail-bookmarks-app/"><img src="images/eye2.png" alt="View Deployment" /></a></li>
                     </ul>
                     <p>This web application allows a user to keep track of their favorite websites. The user can add, delete, and edit the bookmarked tabs, as well as display them by ranking.</p>
                     <ul class="skills">
@@ -48,8 +48,8 @@ function addMain(add) {
                     <h4 class="project-name">Quiz Application</h4>
                     <h5 class="project-description">Test your knowledge about the quizmaker's choice!</h5>
                     <ul class="view-code">
-                        <li class="github-code"><a href="https://github.com/thinkful-ei-gecko/JamesJ-Leon-quiz-app"><img src="images/github-code.png" alt="View on Github" /></a></li>
-                        <li class="deploy-code"><a href="https://thinkful-ei-gecko.github.io/JamesJ-Leon-quiz-app/"><img src="images/eye.png" alt="View Deployment" /></a></li>
+                        <li class="github-code"><a href="https://github.com/thinkful-ei-gecko/JamesJ-Leon-quiz-app"><img src="images/github.png" alt="View on Github" /></a></li>
+                        <li class="deploy-code"><a href="https://thinkful-ei-gecko.github.io/JamesJ-Leon-quiz-app/"><img src="images/eye2.png" alt="View Deployment" /></a></li>
                     </ul>
                     <p>Although originally designed as a small and fun Game of Thrones quiz application, the questions and styles can be ported to any topic by editing the database. This application is fully robust, and the contents can be easily changed for a variety of quizzes.</p>
                     <ul class="skills">
