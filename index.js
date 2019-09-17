40b1ee33ab9f7a146b2829ca8d493434fcff552a
Leon Dail <leondailofficial@gmail.com>
2019-09-16 17:07:19 -0700

    added html to html file instead of js, took away weird transitions, changed how JS works, and added reload/refresh to top

--- index.js
+++ index.js
@@ -1,124 +1,15 @@
-function addMain(add) {
-  $('main').empty();
-  $('.inactive').empty();
-  if (add) {
-    $('main').html(`
-        <article class="information">
-            <h3 class="active" id="bio">bio</h3>
-            <p class="bold">Hi! My name is Leon, and I'm a full-stack web developer.</p>
-            <p>I find myself most proud when I am able to solve a challenging problem. I am confident knowing that I can look at a question from all sides and execute the best solution.</p>
-            <p>My experience with HTML, CSS, and coding goes back many years. In the past, I was the designer and administrator of small, hobby websites, and taught myself how to manage a community and all its coding needs (through trial-and-error, of course). I crafted many programs using a C-type language, and taught myself C++ as well. Now, I am exploring every avenue of JavaScript to further my abilities as a web developer.</p>
-            <p>My biggest passions are designing and creating games; I am always extremely eager to piece together board games, pen and paper games, and internet games alike and test my creativity. Another outlet for my creativity is writing stories!</p>
-                <ul class="skills">
-                    <li class="skill-html"><img src="images/html5.png" alt="HTML 5" /></li>
-                    <li class="skill-css"><img src="images/css.png" alt="CSS 3" /></li>
-                    <li class="skill-javascript"><img src="images/javascript.png" alt="JavaScript" /></li>
-                    <li class="skill-jQuery"><img src="images/jquery.png" alt="jQuery" /></li>
-                    <li class="skill-api"><img src="images/api.png" alt="API" /></li>
-                </ul>
-        </article>
-        <article class="information">
-            <h3 class="active" id="projects">projects</h3>
-
-
-            <div class="project-info">
-                <div class="project-info-text">
-                    <h4 class="project-name">Your Bookmarks</h4>
-                    <h5 class="project-description">Keep tabs of your favorite websites</h5>
-                    <ul class="view-code">
-                        <li class="github-code"><a target="_blank" href="https://github.com/thinkful-ei-gecko/leon-dail-bookmarks-app"><img src="images/github-purple.png" alt="View on Github" /></a></li>
-                        <li class="deploy-code"><a target="_blank" href="https://thinkful-ei-gecko.github.io/leon-dail-bookmarks-app/"><img src="images/eye2.png" alt="View Deployment" /></a></li>
-                    </ul>
-                    <p>This web application allows a user to keep track of their favorite websites. The user can add, delete, and edit the bookmarked tabs, as well as display them by ranking.</p>
-                    <ul class="skills">
-                        <li class="skill-html"><img src="images/html5.png" alt="HTML 5" /></li>
-                        <li class="skill-css"><img src="images/css.png" alt="CSS 3" /></li>
-                        <li class="skill-javascript"><img src="images/javascript.png" alt="JavaScript" /></li>
-                        <li class="skill-jQuery"><img src="images/jquery.png" alt="jQuery" /></li>
-                        <li class="skill-api"><img src="images/api.png" alt="API" /></li>
-                    </ul>
-                </div>
-                <a target="_blank" href="https://thinkful-ei-gecko.github.io/leon-dail-bookmarks-app/" class="project-image-link"><img src="images/bookmark-app.png" alt="Your Bookmarks Screenshot" class="project-image" /></a>
-            </div>
-
-            <div class="divider"></div>
-
-            <div class="project-info">
-                <div class="project-info-text">
-                    <h4 class="project-name">Quiz Application</h4>
-                    <h5 class="project-description">Test your knowledge about the quizmaker's choice!</h5>
-                    <ul class="view-code">
-                        <li class="github-code"><a target="_blank" href="https://github.com/thinkful-ei-gecko/JamesJ-Leon-quiz-app"><img src="images/github-purple.png" alt="View on Github" /></a></li>
-                        <li class="deploy-code"><a target="_blank" href="https://thinkful-ei-gecko.github.io/JamesJ-Leon-quiz-app/"><img src="images/eye2.png" alt="View Deployment" /></a></li>
-                    </ul>
-                    <p>Although originally designed as a small and fun Game of Thrones quiz application, the questions and styles can be ported to any topic by editing the database. This application is fully robust, and the contents can be easily changed for a variety of quizzes.</p>
-                    <ul class="skills">
-                        <li class="skill-html"><img src="images/html5.png" alt="HTML 5" /></li>
-                        <li class="skill-css"><img src="images/css.png" alt="CSS 3" /></li>
-                        <li class="skill-javascript"><img src="images/javascript.png" alt="JavaScript" /></li>
-                        <li class="skill-jQuery"><img src="images/jquery.png" alt="jQuery" /></li>
-                    </ul>
-                </div>
-                <a target="_blank" href="https://thinkful-ei-gecko.github.io/JamesJ-Leon-quiz-app/" class="project-image-link"><img src="images/got-app.png" alt="Quiz Application Screenshot" class="project-image" /></a>
-            </div>
-        </article>
-        <article class="information">
-        <h3 class="active" id="contact">contact</h3>
-        <p>I would be thrilled to write, call, or video chat about any job opportunities. Feel free to contact me through any method below.</p>
-    </article>
-    <footer id="final-links">
-        <ul id="contact-links">
-            <li id="github-link"><a target="_blank" href="https://github.com/ldail"><img src="images/github.png" alt="GitHub" /></a></li>
-            <li id="linkedin-link"><a target="_blank" href="#"><img src="images/linkedin.png" alt="LinkedIn" /></a></li>
-            <li id="email-link"><a target="_blank" href="mailto:leondailofficial@gmail.com"><img src="images/email.png" alt="Email" /></a></li>
-        </ul>
-    <!-- <img src="images/easteregg.png" id="easter-egg" alt="easter egg" /> -->
-    </footer>
-    <!-- <div id="easter-div">
-    <h5>You found the secret easter egg!</h5>
-    <h6>Click below to activate!</h6>
-    <img src="images/easteregg.png" id="second-easter-egg" alt="easter egg" />
-    </div> -->`
-    );
-    $('main').fadeIn('slow');
-    $('.inactive').text('projects');
-  }
-}
-
-function fillNav(add) {
-  if (!add) { //initial loading page
-    $('nav').html(
-      `<ul>
-      <li><a href="#bio">bio</a></li>
-      <li><a href="#projects">projects</a></li>
-      <li><a href="#contact">contact</a></li>
-      </ul>`);
-  }
-  else { //primary webpage (second)
-    $('nav').html(
-      `<ul>
-        <li><a href="#bio">bio</a></li>
-        <li><a href="#projects">projects</a></li>
-        <li><a href="#contact">contact</a></li>
-      </ul>`);
-  }
-}
-
 
 //primary webpage with data
 function showSecondPage() {
   $('h2').html('');
-  fillNav();
-  addMain('add');
+  $('header h2').hide();
+  $('main').show();
   $('*').removeClass('first').addClass('second');
 }
 
 //initial loading page
 function showFirstPage() {
-  $('h1').html('leon<span>dail</span>');
-  $('h2').html('full-stack <span>web developer</span>');
-  fillNav('add');
-  addMain();
+  $('main').hide();
   $('*').removeClass('second').addClass('first');
 }
 
@@ -140,22 +31,28 @@ function eventListeners() {
     $('html,body').animate({scrollTop: name.offset().top-100},'slow');
   });
 
+  //reload takes them back the user back to the loading page
+  if (window.performance) {
+    if (performance.navigation.type === 1) {
+      window.scrollTo(0,0);
+    }
+  }
   //return to loading page
-  $('body').on('click', 'h1', e => showFirstPage());
+  //$('body').on('click', 'h1', e => showFirstPage());
 
   //easter egg listeners
-  $('body').on('click', '#easter-egg', function(e) {
-    $('#easter-div').fadeIn('slow');
-    $('#second-easter-egg').css('transform', 'rotate(1080deg)').css('transition', '10s all');
-  });
-  $('body').on('click', '#second-easter-egg', e => {
-    $('*').css('transform', 'rotate(360deg)').css('transition', '10s all');
-    $('#easter-div').fadeOut();
-    setTimeout(function() {   //calls click event after a certain time
-      $('*').css('transform', '').css('transition', '.5s all');
-      showFirstPage();
-    }, 10000);
-  });
+//   $('body').on('click', '#easter-egg', function(e) {
+//     $('#easter-div').fadeIn('slow');
+//     $('#second-easter-egg').css('transform', 'rotate(1080deg)').css('transition', '10s all');
+//   });
+//   $('body').on('click', '#second-easter-egg', e => {
+//     $('*').css('transform', 'rotate(360deg)').css('transition', '10s all');
+//     $('#easter-div').fadeOut();
+//     setTimeout(function() {   //calls click event after a certain time
+//       $('*').css('transform', '').css('transition', '.5s all');
+//       showFirstPage();
+//     }, 10000);
+//   });
 }
 
 function ready() {
